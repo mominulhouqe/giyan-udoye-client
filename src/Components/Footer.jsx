113ba919ca3e543c34c61e0fc5ccadd9203544d3
Mominul <ulhouqemomin39@gmail.com>
2024-08-21 18:26:15 +0600

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => (
   <div className="bg-gray-800 text-white py-6">
     <div className="text-center">
       <p>&copy; 2024 GiyanUdaye. All rights reserved.</p>
-      <p>Contact us: info@giyanudaye.com</p>
+      <p>Contact us: gganudhay@gmail.com</p>
     </div>
   </div>
 );
